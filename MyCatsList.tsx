56366da6fb5af7e9261bfde6f4e8180feed19339
Michele W Sequeira <miche@alum.mit.edu>
2025-06-14 17:47:58 -0600

    working cat files

--- MyCatsList.tsx
+++ MyCatsList.tsx
@@ -1,19 +1,45 @@
+import { useState } from "react"
 import GetCatsData from "./GetCatsData"
 let dataArray = await GetCatsData()
-let categories = dataArray.map(item => item.title)
+let categories = dataArray.map(item => item) // initial state
 
-export default function MyCatsList() { 
-    return(
-        <div>
-            <button className="btn btn-success">Add a Category</button>
-            {categories.map(category => <p>
-                {category}
-                <button className="btn btn-secondary">Select</button>
-            </p>)}
-        </div>
-    )
-    //return categories // returns a string array of the categories
+type category = {
+    id: number;
+    title: string;
 }
 
+export default function MyCatsList() { 
+    const[catList, setCatList] = useState(categories)
 
-    
+    const addCat = () => {
+        let newItem = {
+            id: 5,
+            title: "New Category"
+        }
+        setCatList( [...catList, newItem] )
+    }
+
+    const delCat = (idToRemove) => {
+        setCatList(catList.filter(x => x.id !== idToRemove))
+    }
+
+    return(
+        <div>
+            <button className="btn btn-success m-3" onClick={addCat}>Add a Category</button>
+            {catList.map(cat => [
+                <p>
+                    <button className="btn btn-danger m-1" onClick={() => delCat(cat.id)}>X</button>
+                    <button className="btn btn-secondary m-1">
+                        {cat.title}
+                    </button>
+                    
+                </p>
+            ]
+            )}
+            
+        </div>
+    )
+}  
+
+{/*  
+ */}
